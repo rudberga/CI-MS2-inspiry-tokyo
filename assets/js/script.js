85eb482a6a15ac48d965a8734a4a63ce731a1552
Alexander Rudberg <alexander_rudberg@hotmail.com>
2020-12-24 21:48:06 +0900

    ADD - new style of radio button in modal which will lead to result

--- assets/js/script.js
+++ assets/js/script.js
@@ -90,3 +90,11 @@ function prep_modal()
 
   });
 }
+
+$(document).ready(function () {
+  $('.radio-group .radio').click(function () {
+  $('.selected .fa').removeClass('fa-check');
+  $('.radio').removeClass('selected');
+  $(this).addClass('selected');
+  });
+  });
