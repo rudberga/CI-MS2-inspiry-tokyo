01f78cfb6feb589879a021ffb156011f139766d0
Alexander Rudberg <alexander_rudberg@hotmail.com>
2020-12-24 12:39:24 +0900

    UPDATE - beautified JS code to make more organized

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 //CREDIT base of modal: https://medium.com/@nerdplusdog/a-how-to-guide-for-modal-boxes-with-javascript-html-and-css-6a49d063987e
 
-let modalBtn = document.getElementById("modal-btn")
-let modal = document.querySelector(".modal")
-let closeBtn = document.querySelector(".close-btn")
+let modalBtn = document.getElementById("modal-btn");
+let modal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-btn");
 
-modalBtn.onclick = function(){
+modalBtn.onclick = function () {
   modal.style.display = "block"
-}
-closeBtn.onclick = function(){
+};
+closeBtn.onclick = function () {
   modal.style.display = "none"
-}
-window.onclick = function(e){
-  if(e.target == modal){
+};
+window.onclick = function (e) {
+  if (e.target == modal) {
     modal.style.display = "none"
-  }
+  };
 }
