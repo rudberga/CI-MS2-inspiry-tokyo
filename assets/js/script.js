b230820d11bc29d2f74291ace54dfd5c7261c912
Alexander Rudberg <alexander_rudberg@hotmail.com>
2020-12-30 12:00:25 +0900

    ADD - JS, HTML and CSS for base of tabs before styling them and adjusting themm

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,3 +98,27 @@ $(document).ready(function () {
   $(this).addClass('selected');
   });
   });
+
+  //--------------------------------- Tabs //
+  //CREDIT tabs: https://www.w3schools.com/howto/howto_js_tabs.asp //
+
+  function openCity(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
